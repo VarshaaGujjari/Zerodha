f897e57a063fec438a83ea5423c41a0760869296
VarshaaGujjari <varshaaa1512@gmail,com>
2026-07-12 01:40:01 +0530

    OpenAccount edit

--- frontend/src/landing_page/OpenAccount.js
+++ frontend/src/landing_page/OpenAccount.js
@@ -11,8 +11,13 @@ function OpenAccount() {
             <h3>Open a Zerodha account</h3>
             <p className='mt-3 mb-4'>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
 
-            <button className='p-2 btn btn-primary fs-5' style
-        = {{width:"20%", margin: "0 auto"}  }> <a href = "http://localhost:3000/signup" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none", color: "white" }}>Sign up for free</a> </button>
+        <a href = "http://localhost:3000/signup" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none", color: "white" }}>
+            <button className='p-2 btn btn-primary fs-5' style = {{width:"20%", margin: "0 auto"}  }> 
+        
+        Sign up for free
+        
+        </button>
+        </a> 
 
         </div>
         </div>
--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -9,8 +9,13 @@ function Hero() {
 
                 <h1 className='mt-5'>Invest in Everything</h1>
         <p>Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds and more.</p>
-        <button className='p-2 btn btn-primary fs-5 ' style
-        = {{width:"20%", margin: "0 auto"}}><a href = "http://localhost:3000/signup" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none", color: "white" }}>Sign up for free</a></button>
+        <a href = "http://localhost:3000/signup" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none", color: "white" }}>
+        <button className='p-2 btn btn-primary fs-5 ' style = {{width:"20%", margin: "0 auto"}}>
+            
+            Sign up for free
+            </button>
+            </a>
+            
             </div>
             
         </div>
